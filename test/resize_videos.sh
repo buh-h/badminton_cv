b1af2dd4a708f1bc8b8ed81801dcb99c3d5e0c81
buh-h <maxwellwang22@gmail.com>
2026-01-07 00:12:50 -0600

    added script to resize videos for TracknetV3

---
+++ test/resize_videos.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+INPUT_DIR="$(pwd)"
+OUTPUT_DIR="$(pwd)/resized}"
+WIDTH=512
+HEIGHT=288
+CODEC="libx264"
+CRF=23
+PRESET="medium"
+
+while getopts "i:o:w:h:c" opt; do
+  case $opt in
+    i) INPUT_DIR="OPTARG" ;;
+    o) OUTPUT_DIR="OPTARG" ;;
+    w) WIDTH="OPTARG" ;;
+    h) HEIGHT="OPTARG" ;;
+    c) CODEC="OPTARG" ;;
+    *) echo "Unknown flag"; exit 1 ;;
+  esac
+done
+
+mkdir -p "$OUTPUT_DIR"
+
+for file in "$INPUT_DIR"/*.mp4; do
+  [ -e file] || continue
+  filename=$(basename "$file")
+  output="$OUTPUT_DIR/$filename"
+
+  echo "Resizing $filename"
+
+  SCALE="-vf scale=$WIDTH:$HEIGHT"
+  
+  ffmpeg -r "$file" \
+    $SCALE \
+    -c:v "$CODEC" \
+    -preset "$PRESET" \
+    -crf "$CRF" \
+    -c:a copy \
+    "$output"
